ea313f7788717d5d7b82360bc5dce34f08442a3a
Ilya Pestrikov <ilya.pestrikov2@gmail.com>
2020-09-20 17:00:33 +0300

    Changed xp and coins rewards for quests

--- app/src/main/java/com/elli0tt/rpg_life/domain/model/Difficulty.kt
+++ app/src/main/java/com/elli0tt/rpg_life/domain/model/Difficulty.kt
@@ -2,10 +2,10 @@ package com.elli0tt.rpg_life.domain.model
 
 enum class Difficulty(val xpIncrease: Int, val coins: Int) {
     VERY_EASY(100, 1),
-    EASY(500, 5),
-    NORMAL(1000, 10),
-    HARD(2000, 20),
-    VERY_HARD(5000, 50),
+    EASY(250, 3),
+    NORMAL(500, 5),
+    HARD(1000, 10),
+    VERY_HARD(2000, 20),
     IMPOSSIBLE(10000, 100),
     NOT_SET(0, 0);
 }
--- app/src/main/java/com/elli0tt/rpg_life/presentation/screen/add_skills_to_quest/AddSkillsToQuestViewHolder.kt
+++ app/src/main/java/com/elli0tt/rpg_life/presentation/screen/add_skills_to_quest/AddSkillsToQuestViewHolder.kt
@@ -1,41 +1,54 @@
 package com.elli0tt.rpg_life.presentation.screen.add_skills_to_quest
 
 import android.view.View
-import android.widget.SeekBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.elli0tt.rpg_life.R
 import com.elli0tt.rpg_life.domain.model.AddSkillData
+import com.google.android.material.slider.Slider
 
 class AddSkillsToQuestViewHolder(itemView: View,
                                  onXpPercentageSeekBarTouchStopListener: AddSkillsToQuestAdapter.OnXpPercentageSeekBarTouchStopListener)
     : RecyclerView.ViewHolder(itemView) {
 
     private val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
-    private val xpPercentageSeekBar: SeekBar = itemView.findViewById(R.id.xp_percentage_seek_bar)
+    private val xpPercentageSlider: Slider = itemView.findViewById(R.id.xp_percentage_seek_bar)
     private val xpValueTextView: TextView = itemView.findViewById(R.id.xp_text_view)
 
     init {
-        xpPercentageSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                xpValueTextView.text = itemView.context.getString(R.string.add_skills_to_quest_xp_percent, progress)
-            }
+        xpPercentageSlider.addOnChangeListener { slider, value, fromUser ->
+            xpValueTextView.text = itemView.context.getString(R.string.add_skills_to_quest_xp_percent, value.toInt())
+        }
 
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+        xpPercentageSlider.addOnSliderTouchListener(object : Slider.OnSliderTouchListener {
+            override fun onStartTrackingTouch(slider: Slider) {
                 //do nothing
             }
 
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                onXpPercentageSeekBarTouchStopListener.onTouchStop(adapterPosition, seekBar!!.progress)
+            override fun onStopTrackingTouch(slider: Slider) {
+                onXpPercentageSeekBarTouchStopListener.onTouchStop(adapterPosition, slider.value.toInt())
             }
         })
+//        xpPercentageSlider.setOnsetOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+//            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+//                xpValueTextView.text = itemView.context.getString(R.string.add_skills_to_quest_xp_percent, progress)
+//            }
+//
+//            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+//                //do nothing
+//            }
+//
+//            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+//                onXpPercentageSeekBarTouchStopListener.onTouchStop(adapterPosition, seekBar!!.progress)
+//            }
+//        })
     }
 
     fun bind(skill: AddSkillData) {
         nameTextView.text = skill.name
-        xpPercentageSeekBar.progress = skill.xpPercentage
+        xpPercentageSlider.value = skill.xpPercentage.toFloat()
         xpValueTextView.text = itemView.context.getString(R.string.add_skills_to_quest_xp_percent,
-                xpPercentageSeekBar.progress)
+                xpPercentageSlider.value.toInt())
 
     }
 
